c5732b25eed207257e04d78553823f561cec6fb1
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-09-29 04:12:34 +1300

    (1) Creating submit article button (Create) button to navigate to submit page as submit form.

--- speed-frontend/src/app/search/page.tsx
+++ speed-frontend/src/app/search/page.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import Header from "../component/Header";
+import { useRouter } from "next/navigation";
 export default function SearchView() {
+  /**
+   * @author @Seongrok-Shin
+   * Description
+   * @returns {any}
+   */
+
+  const router = useRouter();
+
+  const submitArticle = () => {
+    router.push("/submit");
+  };
+
   return (
     <>
       <Header />
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is search view</h1>
+        <div className="flex flex-col items-center">
+          <h1>This is search view</h1>
+          <button
+            onClick={submitArticle}
+            className="w-full rounded-xl border-2 border-gray-300 focus:outline-none
+          focus:border-indigo-500 text-base px-4 py-2"
+          >
+            Submit Article
+          </button>
+        </div>
       </div>
     </>
   );
