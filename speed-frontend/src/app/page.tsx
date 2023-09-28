78ccde70fa1588e76f6cb3c4ad574e92645987de
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-09-29 03:23:13 +1300

    Revert "(8) Creating header component"
    This reverts commit 32d88a254ea4b5b51ad229719a5c32c4e65d6586.

--- speed-frontend/src/app/page.tsx
+++ speed-frontend/src/app/page.tsx
@@ -1,13 +1,11 @@
 import Image from "next/image";
-import Header from "./nav/Header";
 
 export default function Home() {
   return (
-    <>
-      <Header />
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is home</h1>
+        <h1>Hello, World!</h1>
       </div>
-    </>
+    </main>
   );
 }
