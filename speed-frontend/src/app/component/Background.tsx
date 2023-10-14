ce53221dc68d4dbf164671817cd01c6e821053fc
Seongrok-Shin <82435505+Seongrok-Shin@users.noreply.github.com>
2023-10-14 18:00:24 +1300

    Re-touching header and homepage (Create) background component (Update) Header.tsx - remove onClick function as it changed into link - changed color (Update) page.tsx(Home) - Remove default - Add Background - Change UIs

---
+++ speed-frontend/src/app/component/Background.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const image = () => {
+  return (
+    <div>
+      <img src="/backgroundImage.png" alt="background" />
+    </div>
+  );
+};
+
+export default image;
--- speed-frontend/src/app/page.tsx
+++ speed-frontend/src/app/page.tsx
@@ -1,14 +1,66 @@
 "use client";
 
+import React from "react";
 import Header from "./component/Header";
+import Background from "./component/Background";
+import { useRouter } from "next/navigation";
+const Home = () => {
+  const router = useRouter();
 
-export default function Home() {
+  const searchPage = () => {
+    router.push("/search");
+  };
+  const moderatorPage = () => {
+    router.push("/moderator");
+  };
+  const analystPage = () => {
+    router.push("/analyst");
+  };
+
   return (
-    <>
+    <div className="bg-[#0332CB]">
       <Header />
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <h1>This is home</h1>
+      <div className="overflow-hidden">
+        <div className="absolute w-full font-bold font-sans py-48 px-52 text-white text-3xl leading-4">
+          <p className="py-4">Welcome to</p>
+          <p className="py-4">SPEED DATABASE</p>
+          <p className="py-4">MY BLOG ON EDUCATION</p>
+          <p className="py-4">AND TECHNOLOGY</p>
+        </div>
+        <div className="absolute py-48 inset-x-0 font-sans text-white text-xl leading-4 items-center flex flex-col">
+          <button
+            className="font-bold py-8 px-10 hover:bg-[#0332CB] bg-white rounded-full bg-opacity-25"
+            onClick={searchPage}
+          >
+            <img src="/magnifier.png" className="py-2 bg-opacity-0" />
+            Searching View
+          </button>
+        </div>
+        <div className="absolute bottom-24 inset-x-0 font-sans text-white text-xl leading-4 items-center flex flex-col">
+          <button
+            className="font-bold py-8 px-10 hover:bg-[#0332CB] bg-white rounded-full bg-opacity-25"
+            onClick={moderatorPage}
+          >
+            <img src="/magnifier.png" className="py-2 bg-opacity-0" />
+            Moderator View
+          </button>
+        </div>
+        <div className="absolute bottom-[240px] ml-[1300px] font-sans text-white text-xl leading-4 items-center flex flex-col">
+          <button
+            className="font-bold py-8 px-10 hover:bg-[#0332CB] bg-white rounded-full bg-opacity-25"
+            onClick={analystPage}
+          >
+            <img src="/magnifier.png" className="py-2 bg-opacity-0" />
+            Analysting View
+          </button>
+        </div>
+        <Background />
+        <div className="absolute w-full py-2.5 bottom-0 inset-x-0 text-white text-xs text-center leading-4">
+          © 2023 by SPEED DATABASE Powered and secured by AUT
+        </div>
       </div>
-    </>
+    </div>
   );
-}
+};
+
+export default Home;
